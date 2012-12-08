792e8eea1ea1bee37ab004f36420df3eae7bc55c
Yuri Khan <yurivkhan@gmail.com>
2012-12-09 00:36:26 +0700

    Implement basic canvas and model

--- jotuml.hs
+++ jotuml.hs
@@ -2,13 +2,24 @@ import Control.Exception.Base
 import Control.Monad
 import Control.Monad.IO.Class
 import Control.Applicative
+import Data.IORef
 import Debug.Trace
+import Graphics.Rendering.Cairo
 import Graphics.UI.Gtk
 import Graphics.UI.Gtk.Builder
 import Graphics.UI.Gtk.Windows.MessageDialog
 
+-- workaround
+modifyIORef'' :: IORef a -> (a -> a) -> IO ()
+modifyIORef'' ref f = do
+    x <- readIORef ref
+    let x' = f x
+    x' `seq` writeIORef ref x'
+
+
 data UI = UI { builder :: Builder,
                window :: Window,
+               layout :: Layout,
                aboutBox :: Dialog,
                fileNewItem :: MenuItem,
                fileOpenItem :: MenuItem,
@@ -22,7 +33,103 @@ data UI = UI { builder :: Builder,
                helpAboutItem :: MenuItem
              }
 
-loadUI :: IO UI
+type ModelRef = IORef Model
+
+data Model = Model { modelVertices :: [Vertex],
+                     -- TODO: modelEdges :: [Edge],
+                     modelDiagram :: Diagram }
+
+newModel = Model [] newDiagram
+
+data Vertex = Vertex { vertexKind :: VertexKind,
+                       vertexCompartments :: [Compartment] }
+
+data VertexKind = Class -- TODO: | Interface | Package | Node | Component | Actor | UseCase
+
+newVertex = Vertex Class [Compartment "Hello"]
+
+data Compartment = Compartment { compartmentText :: String }
+
+measureCompartment :: PangoLayout -> (Double, Double, [Double]) -> Compartment -> Render (Double, Double, [Double])
+measureCompartment layout (oldWidth, oldHeight, oldRules) compartment = do
+    liftIO $ layoutSetText layout $ compartmentText compartment
+    (Rectangle inkx _ inkw _, Rectangle _ _ _ logh) <- liftIO $ layoutGetPixelExtents layout
+    let width = 4 + (abs inkx) + inkw
+        height = 6 + logh
+        newHeight = oldHeight + fromIntegral height
+    return (max oldWidth $ fromIntegral width, newHeight, newHeight : oldRules)
+
+renderCompartment :: PangoLayout -> Double -> Double -> (Double, Compartment) -> Render ()
+renderCompartment layout left top (y, compartment) = do
+    liftIO $ layoutSetText layout $ compartmentText compartment
+    liftIO $ layoutContextChanged layout
+    translate (left + 2) (top + y + 3)
+    showLayout layout
+    identityMatrix
+
+
+data Diagram = Diagram { diagramViews :: [View] }
+
+newDiagram = Diagram []
+
+renderDiagram :: Diagram -> Render ()
+renderDiagram diagram = mapM_ renderView $ diagramViews diagram
+
+data View = VertexView { viewVertex :: Vertex,
+                         viewCx :: Double,
+                         viewCy :: Double,
+                         viewIsNew :: Bool }
+
+renderView :: View -> Render ()
+renderView (VertexView v cx cy _) = do
+    setSourceRGB 0 0 0
+    setAntialias AntialiasSubpixel
+    context <- liftIO $ cairoCreateContext Nothing
+    liftIO $ fontDescriptionFromString "sans 10" >>= contextSetFontDescription context
+    layout <- liftIO $ layoutEmpty context
+    liftIO $ layoutSetIndent layout (-16384)
+    (width, height, rules) <- foldM (measureCompartment layout) (0, 0, []) $ vertexCompartments v
+    let left = cx - width / 2
+        top = cy - height / 2
+        right = cx + width / 2
+    setLineWidth 1
+    rectangle left top width height
+    mapM (drawRule left right) (init rules)
+    stroke
+    mapM (renderCompartment layout left top) (zip (0:reverse rules) $ vertexCompartments v)
+    return ()
+    where drawRule leftx rightx y = do
+              moveTo leftx y
+              lineTo rightx y
+
+addNewClassView :: Double -> Double -> Model -> Model
+addNewClassView x y oldModel = Model (newVertex:oldVertices) newDiagram where
+    oldVertices = modelVertices oldModel
+    newDiagram = Diagram $ newView:oldViews
+    newView = VertexView newVertex x y True
+    oldViews = diagramViews $ modelDiagram oldModel
+
+
+layoutButtonPressed :: UI -> ModelRef -> EventM EButton Bool
+layoutButtonPressed ui modelRef = tryEvent $ do
+    (x, y) <- eventCoordinates
+    liftIO $ modifyIORef'' modelRef $ addNewClassView x y
+    liftIO $ widgetQueueDraw $ layout ui
+    return ()
+
+layoutButtonReleased :: UI -> ModelRef -> EventM EButton Bool
+layoutButtonReleased ui modelRef = tryEvent $ do
+    return ()
+
+layoutExposed :: UI -> ModelRef -> EventM EExpose Bool
+layoutExposed ui modelRef = tryEvent $ do
+    drawWindow <- eventWindow
+    model <- liftIO $ readIORef modelRef
+    liftIO $ renderWithDrawable drawWindow $ renderDiagram $ modelDiagram model
+    return ()
+
+
+loadUI :: IO ()
 loadUI = do
     builder <- builderNew
     builderAddFromFile builder "jotuml.ui"
@@ -31,6 +138,8 @@ loadUI = do
     on window objectDestroy mainQuit
     on window deleteEvent $ do
         liftIO $ confirm (Just window) quitConfirmation (return False) (return True)
+
+    layout <- builderGetObject builder castToLayout "layout"
 
     aboutDialog <- builderGetObject builder castToDialog "about_dialog"
 
@@ -47,12 +156,20 @@ loadUI = do
 
     helpAboutItem <- builderGetObject builder castToMenuItem "help_about_item"
 
+    let ui = UI builder window layout aboutDialog fileNewItem fileOpenItem fileSaveItem
+             fileSaveAsItem fileQuitItem editCutItem editCopyItem editPasteItem editDeleteItem
+             helpAboutItem
+
+    modelRef <- newIORef newModel
+
     on fileQuitItem menuItemActivate $ confirm (Just window) quitConfirmation mainQuit $ return ()
     on helpAboutItem menuItemActivate $ dialogRun aboutDialog `finally` widgetHide aboutDialog >> return ()
 
-    return $ UI builder window aboutDialog fileNewItem fileOpenItem fileSaveItem
-        fileSaveAsItem fileQuitItem editCutItem editCopyItem editPasteItem editDeleteItem
-        helpAboutItem
+    on layout buttonPressEvent $ layoutButtonPressed ui modelRef
+    on layout buttonReleaseEvent $ layoutButtonReleased ui modelRef
+    on layout exposeEvent $ layoutExposed ui modelRef
+
+    return ()
 
 confirm :: Maybe Window -> String -> IO a -> IO a -> IO a
 confirm parent text action inaction = do
@@ -67,5 +184,5 @@ quitConfirmation = "Do you really want to quit?"
 main :: IO ()
 main = do
     initGUI
-    ui <- loadUI
+    loadUI
     mainGUI
